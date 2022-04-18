b544d3551eef11f914fc2f5583b3f7a7f1bf5b27
Ayon <ayonjodder177@gmail.com>
2022-04-18 16:51:33 +0600

    Card updated

--- src/Components/Home/SingleService/SingleService.js
+++ src/Components/Home/SingleService/SingleService.js
@@ -7,12 +7,12 @@ const SingleService = (props) => {
     return (
         <div className='custom-shadow py-10 rounded-md'>
 
-            <div className="max-w-sm  rounded-lg border text-center">
+            <div className="rounded-lg border text-center">
                 <img className="rounded-t-lg w-2/3 mx-auto" src={img} alt="" />
                 <div className="p-5">
-                    <h1>{service}</h1>
+                    <h1 className='md:text-2xl text-lg font-medium mb-2'>{service}</h1>
 
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{details}</p>
+                    <p className="mb-3 text-sm md:text-base font-normal text-gray-700 dark:text-gray-400">{details.slice(0,100)}...</p>
                     <p>Price ${price}</p>
 
                 </div>
